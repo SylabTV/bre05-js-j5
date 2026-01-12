ffbe5acb78635e06f6ef9339470b01c83930c621
sylabtv <sylab.studio@gmail.com>
2026-01-12 10:55:32 +0100

    ajouter un élément avec des attributs﻿

---
+++ exercice-3/index.js
@@ -0,0 +1,5 @@
+let body = document.getElementsByTagName("body")[0];
+let link = document.createElement("a");
+link.textContent = "L'article de Wikipédia sur le Lorem Ipsum";
+link.setAttribute("href", "https://fr.wikipedia.org/wiki/Lorem_ipsum");
+body.appendChild(link);
